05113723da128f94a022a0232c2dbd14933ef965
Andre <andremayerp@gmail.com>
2020-04-22 12:21:34 -0300

    Criando uma pasta publica
    Essa pasta serve para o servidor conseguir acessa-la. Chamando arquivos, por exemplo na tag <link href="" />

--- handlebars/app.js
+++ handlebars/app.js
@@ -11,8 +11,14 @@ app.set('view engine', 'handlebars');
 //--CONFIGURANDO BODY-PARSER
 // support parsing of application/json type post data
 app.use(bodyParser.json());
-//support parsing of application/x-www-form-urlencoded post data
+// support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({extended: false}));
+
+//configurando os publicos do express, aqui que ele consulta seu arquivos
+//https://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+console.log(__dirname)
 
 //Conexao com o banco
 const sequelize = new Sequelize('nodejs', 'root', 'root', {
