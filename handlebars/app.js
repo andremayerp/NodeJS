4a04848e4c38ec70b23a4a90c1b6596d9ef7310e
Andre <andremayerp@gmail.com>
2020-04-24 15:28:59 -0300

    Ajustes no app.js
    Refatorando app.js
    Iconsbar exemplo(font-awesome).

--- handlebars/app.js
+++ handlebars/app.js
@@ -3,6 +3,8 @@ const app = express();
 const handlebars = require('express-handlebars');
 const Sequelize = require('sequelize')
 const bodyParser = require('body-parser');
+const port = 8181;
+const url = require('url');
 
 //--CONFIGURANDO ENGINE TEMPLATE
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
@@ -26,18 +28,41 @@ const sequelize = new Sequelize('nodejs', 'root', 'root', {
     dialect: 'mysql'
 })
 
-app.get('/form', function(req, res){
+let getForm = function(req, res){
     res.render('formulario');
-})
+}
 
-app.post('/recebendoForm', function(req, res){
-    res.send(req.body.nome);
-})
+let postRecebendoForm = function(req, res){
+    let nome = req.body.nome;
+    let texto = req.body.texto;
+    res.redirect('/test?nome=' + nome + "&texto=" + texto);
+    //o redirect nao leva o body do req
+} //usar enconding?
 
-app.get('/test', (req, res) =>{
-    res.render('test');
-});
+let getTest = (req, res) =>{
+    let objUrl = url.parse(req.url, true);
+    
+    res.send("Nome = " + objUrl.query.nome + "<br/>" 
+    + "Texto = " +objUrl.query.texto);
+}
 
-app.listen(8181, function(){
+let getJavaScript = (req, res) =>{
+    res.render('animacao');
+}
+
+let icons = (req, res) =>{
+    res.render('iconsbar');
+}
+
+
+
+app.post('/recebendoForm', postRecebendoForm);
+
+app.get('/form', getForm);
+app.get('/test', getTest);
+app.get('/javascript', getJavaScript);
+app.get('/iconsbar', icons);
+
+app.listen(port, function(){
     console.log('Server running...');
 })
